399673ca9fd5b946d2e71741ba5e914ba6f0560b
jiwonjung <4dakto@gmail.com>
2022-02-28 14:30:25 +0900

    issues

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,27 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export const Form = ({ todoList, setTodoList }) => {
-  const inputRef = useRef();
   const [text, setText] = useState(null);
+
+  const clearUserInput = () => setText("");
 
   const addTodoList = (index, text) => {
     if (text === "") return;
 
     const newTodoList = [...todoList, { id: index, todo: text }];
-    setTodoList(newTodoList);
 
-    inputRef.current.value = "";
+    setTodoList(newTodoList);
+    clearUserInput()
   };
 
-  const id = Date.now();
+  const inputHandler = ({ target }) => setText(target.value)
+  const onClickHander = () => addTodoList(uuidv4(), text)
 
   return (
     <div>
-      <input ref={inputRef} onChange={({ target }) => setText(target.value)} />
-      <button onClick={() => addTodoList(id, text)}>추가</button>
+      <input value={text} onChange={inputHandler} />
+      <button onClick={onClickHander}>추가</button>
     </div>
   );
 };
