1f3ddeea0719737edb01f610d987caf82e27bc0f
wilson-jung <69133229+wilson-jung@users.noreply.github.com>
2022-03-06 13:07:01 +0900

    issues (#1)
    Co-authored-by: jiwonjung <4dakto@gmail.com>

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 export const List = ({ todo, id, removeTodoList }) => {
+  const removeTodoHandler = () => removeTodoList(id);
+
   return (
     <li>
       <p>{todo}</p>
       <p>{id}</p>
-      <button onClick={() => removeTodoList(id)}>삭제</button>
+      <button onClick={removeTodoHandler}>삭제</button>
     </li>
   );
 };
